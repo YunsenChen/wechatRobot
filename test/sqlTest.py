4014240acc7efa5a48dc428441877e2ade82d6cb
YUNSEN-CHEN <1316568848@qq.com>
2023-12-28 15:58:15 +0800

    sql

--- test/sqlTest.py
+++ test/sqlTest.py
@@ -1,23 +1,29 @@
-# -*- coding:utf-8 -*-
-from bs4 import BeautifulSoup
-import requests
-import re
-import MySQLdb
-# 解决出现的写入错误
 
+import pymysql
+import configparser
+config = configparser.ConfigParser()
+config.read('config.ini')
+# 从配置文件中获取数据库连接信息
+host = config.get('database', 'host')
+user = config.get('database', 'user')
+passwd = config.get('database', 'passwd')
+port = config.getint('database', 'port')
+# 连接到数据库
+db = pymysql.connect(host=host, user=user, passwd=passwd, port=port)
+# 使用 cursor() 方法创建一个游标对象
+cursor = db.cursor()
+# 查询所有数据库的名称
+cursor.execute("SHOW DATABASES")
+# 获取所有数据库的名称
+all_databases = cursor.fetchall()
+# 打印所有数据库的名称
+for database in all_databases:
+    print(database[0])
+# 如果test数据库不存在，则创建它
+# if ('wechatDB',) not in all_databases:
+#     cursor.execute("CREATE DATABASE WechatDB")
+#     print("Database 'WechatDB' created")
+# 关闭游标和数据库连接
+cursor.close()
+db.close()
 
-
-print('连接到mysql服务器...')
-conn = MySQLdb.connect(host='127.0.0.1', user='root', passwd='123mysql', db='onefive', charset='utf8')
-print('连接上了!')
-cur = conn.cursor()
-# 判断表是否存在，若存在则删除此表
-cur.execute("DROP TABLE IF EXISTS AGENT")
-# 创建表
-sql = """CREATE TABLE test(
-                 title  CHAR(20),
-                 sec_title  CHAR(20),
-                 content  VARCHAR(6499))"""
-cur.execute(sql)
-conn.commit()
-conn.close()
